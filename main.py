78a83a2fa68e2927ddd5cd0ecb3fb32b7e899ba8
bray0j <123158695+bray0j@users.noreply.github.com>
2023-03-21 23:15:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from subprocess import run
 import logging
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
 from os.path import exists
 import os
 import re
@@ -9,8 +15,13 @@ from fastapi.responses import FileResponse
 
 from starlette.background import BackgroundTask
 
+
 def cleanup(path: str) -> None:
-    os.remove(path)
+    try:
+        os.remove(path)
+    except Exception:
+        logger.exception(f'Error during remove of path: {path}')
+
 
 valid_key_and_type = re.compile(r'[A-Za-z0-9]+')
 
@@ -20,26 +31,36 @@ app = FastAPI()
 async def convert(file: UploadFile, key: str, cache = False, outputtype: str = 'pdf'):
     try:
         if not valid_key_and_type.fullmatch(key):
-            logging.info(f'key is invalid. should contain letters and digits only')
+            logger.info(f'key is invalid. should contain letters and digits only')
             raise HTTPException(status_code=422, detail=f'key is invalid. should contain letters and digits only')
         
         if not valid_key_and_type.fullmatch(outputtype):
-            logging.info(f'outputtype is invalid. should contain letters and digits only')
+            logger.info(f'outputtype is invalid. should contain letters and digits only')
             raise HTTPException(status_code=422, detail=f'outputtype is invalid. should contain letters and digits only')
 
         # Check if key valid, and outputtype valid
         converted_file_path = f'/tmp/{key}.{outputtype}'
+        already_exist = True
 
         if not exists(converted_file_path):
-            run(['unoconvert', '--convert-to', outputtype, '-', converted_file_path], stdin=file.file)
+            already_exist = False
+            logger.info(f'key does not exist already, running unoconvert on the file.')
 
-        if cache:
+            try:
+                run(['unoconvert', '--convert-to', outputtype, '-', converted_file_path], stdin=file.file)
+            except Exception:
+                logger.exception(f'Error during unoconvert run')
+
+        if cache or already_exist:
+            logger.debug('returning cached file')
             return FileResponse(converted_file_path)
+    
+        logger.debug(f'cache not exist, returning the file {converted_file_path} and then removing it.')
 
         return FileResponse(
             converted_file_path,
             background=BackgroundTask(cleanup, converted_file_path)
         )
     except Exception:
-        logging.exception(f'Failed converting {file.filename}')
+        logger.exception(f'Failed converting {file.filename}')
         raise HTTPException(status_code=500, detail=f'Failed converting {file.filename}')
